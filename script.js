8588359bcda3bc819e8f6a988e92b9c68c0e3293
2yashraj6 <88868010+2yashraj6@users.noreply.github.com>
2021-09-03 20:20:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let upload = document.getElementById('upload');
+let submit = document.getElementById('submit');
+
+$.getJSON("https://cors.bridged.cc/https://api.openvisionapi.com/api/v1/detection to avoid ",function(data){
+    console.log(data);
+});
+
